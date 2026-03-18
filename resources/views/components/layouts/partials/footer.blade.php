28ffb487bc82c0a7ea9cd226f7e3b3de963087b0
Cegem-360 <cegem360workplace@gmail.com>
2026-03-18 19:29:57 +0100

    Remove Ministry Team Application link from footer

--- resources/views/components/layouts/partials/footer.blade.php
+++ resources/views/components/layouts/partials/footer.blade.php
@@ -52,7 +52,6 @@
                 <h4 class="font-semibold mb-4">Registration</h4>
                 <ul class="space-y-3">
                     <li><a href="{{ route('register') }}" class="text-white/50 hover:text-white text-sm transition-colors">Attendee Registration</a></li>
-                    <li><a href="{{ route('ministry-team') }}" class="text-white/50 hover:text-white text-sm transition-colors">Ministry Team Application</a></li>
                     <li><a href="{{ route('volunteer') }}" class="text-white/50 hover:text-white text-sm transition-colors">Volunteer Application</a></li>
                     <li><a href="mailto:info@iriseuroperevival.com" class="text-white/50 hover:text-white text-sm transition-colors">Contact Us</a></li>
                 </ul>
